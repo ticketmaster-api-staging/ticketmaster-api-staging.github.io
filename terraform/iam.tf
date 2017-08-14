5eeadda39bc34435bd886e7573168acf77c55bfc
Oleksandr Kovalov <oleksandr_kovalov@epam.com>
2017-08-14 17:35:25 +0300

    terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -70,43 +70,6 @@ resource "aws_iam_role_policy" "AmazonCloudWatchFullAccess" {
 EOF
 }
 
-resource "aws_iam_policy" "S3LogBucketFullAccess" {
-  name        = "${lower(format("%s.%s.%s.%s.%s.tmaws", var.product_code_tag, var.account_tag, var.product_name, var.environment_tag, var.aws_region))}-logs-${var.inventory_code_tag}-S3LogBucketFullAccess"
-  path        = "/"
-  description = "Allow full access to the product s3 bucket arn:aws:s3:::${var.product_code_tag}.${var.account_tag}.${var.product_name}.${var.environment_tag}.${var.aws_region}.tmaws-logs"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": "arn:aws:s3:::${var.s3_bucket_logs}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:DeleteObject"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${var.s3_bucket_logs}/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "EC2S3LogsBucketFullAccess" {
-  name   = "${format("%s.%s.%s.tmaws", var.product_code_tag, var.environment_tag, var.aws_region)}-S3LogBucketFullAccess"
-  role   = "${aws_iam_role.default.id}"
-  policy = "${aws_iam_policy.S3LogBucketFullAccess.policy}"
-}
-
-
 #
 # This is required so EC2 instance has an instance profile attached
 #
