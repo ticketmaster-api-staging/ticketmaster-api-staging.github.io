8df1411f7e69e86141c5b8d9933af8ae91b38b02
Oleksandr Kovalov <oleksandr_kovalov@epam.com>
2017-08-28 19:47:52 +0300

    tmp-removed

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function getURL(srcPath, res) {
 }
 
 function getRequestURI(userId) {
-  var res = syncrequest('GET', 'https://test-livenation.devportal.apigee.com/open-platform/user/' + userId + '/roles');
+  var res = syncrequest('GET', 'https://dev-livenation.devportal.apigee.com/open-platform/user/' + userId + '/roles');
   // var res = syncrequest('GET', 'https://pantheon.staging.ticketmaster.com/open-platform/user/' + userId + '/roles');
   return res.getBody().toString();
 }
